90393b4e7bd30bef9bf45a52055b5e2baf1a158d
ibrah3m <91458548+ibrah3m@users.noreply.github.com>
2022-12-15 16:14:15 -0800

    Update and rename .gitpod-init.sh to .gitpod.sh

--- .gitpod-init.sh
+++
@@ -1,7 +0,0 @@
-mysql -u root -e "create database test" &&
-cp .env.example .env &&
-sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env &&
-sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env &&
-composer install &&
-npm i && npm run dev &&
-php artisan migrate
